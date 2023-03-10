63bc2221ad255e08fd577aaeaab2233a16d3e22d
Ali-Tabash <mta20mta@gmail.com>
2023-03-10 21:04:39 +0200

    mt

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>site1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+            <h1>welcome</h1>
+            <h1><?php echo $name ?></h1>
+            <h1><?php echo $text ?></h1>
+
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('about',function(){
+    // return view('about');
+    // بثلاث طرق لارسال البيانات الى صفحة
+    // (view)
+    // 1. with 2.compact 3. array
+
+    $name = 'ali';
+    $text = 'new text';
+
+    // return view('about')->with('name',$name) ->with('text',$text);
+     //  (laravel)الطريقة الاولى من (with)
+    return view('about', compact('name','text')); // (php) الطريقة الاكثر استخداما وهي من
+    // فقط اعطيها اسم المتغير compact
+    return view('about',[ //    كتابة بشكل صريحarray 3طريقة
+        'name'=> $name,
+        'text'=> $text
+    ]);
+});
